9c2182596b8d12181de1890b9941a03686f86998
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-04-23 08:01:34 +0200

    organize folders

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,25 @@ import '../styles/App.scss';
 function App() {
   return (
     <div>
-      <h1>Hola mundo!</h1>
+      <h1>pomodoro
+  short break
+  long break
+
+  start 
+  pause
+  restart
+
+  Settings
+
+  Time (minutes)
+  pomodoro
+  short break
+  long break
+
+  Font
+  Color
+
+  Apply</h1>
     </div>
   );
 }
