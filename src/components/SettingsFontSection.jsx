059c99ae5d36ebcccd881b3d3c727041ee7e8f21
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-04-27 12:17:31 +0200

    add font state

--- src/components/SettingsFontSection.jsx
+++ src/components/SettingsFontSection.jsx
@@ -5,32 +5,49 @@ const SettingsFontSection = () => {
     const [firstFont, setFirstFont] = useState(true)
     const [secondFont, setSecondFont] = useState(false)
     const [thirdFont, setThirdFont] = useState(false)
+    
+    const [classFirstFont, setClassFirstFont] = useState('kumbh-sans-font')
+    const [classSecondFont, setClassSecondFont] = useState('')
+    const [classThirdFont, setClassThirdFont] = useState('')
 
     const handleClickFirstFont = () => {
         setFirstFont(true)
         setSecondFont(false)
         setThirdFont(false)
+        setClassFirstFont('kumbh-sans-font')
+        setClassSecondFont('')
+        setClassThirdFont('')
     }
 
     const handleClickSecondFont = () => {
         setSecondFont(true)
         setFirstFont(false)
         setThirdFont(false)
+        setClassSecondFont('roboto-slab-font')
+        setClassFirstFont('')
+        setClassThirdFont('')
     }
 
     const handleClickThirdFont = () => {
         setThirdFont(true)
         setFirstFont(false)
         setSecondFont(false)
+        setClassThirdFont('space-mono-font')
+        setClassFirstFont('')
+        setClassSecondFont('')
     }
 
     let toggleFirstFont = firstFont ? 'active-font' : 'inactive-font'
     let toggleSecondFont = secondFont ? 'active-font' : 'inactive-font'
     let toggleThirdFont = thirdFont ? 'active-font' : 'inactive-font'
+
+    let updateFirstFont = classFirstFont ? 'kumbh-sans-font' : ''
+    let updateSecondFont = classSecondFont ? 'roboto-slab-font' : ''
+    let updateThirdFont = classThirdFont ? 'space-mono-font' : ''
     
     return (
         <section className='settings-container-font'>
-            <h3 className='settings-container-font-title'>
+            <h3 className={`settings-container-font-title ${updateFirstFont} ${updateSecondFont} ${updateThirdFont}`}>
             Font
             </h3>
             <div className='settings-container-font-container'>
