6924178831c7870bf5bf1889605fa9a9ce400327
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-05-03 05:39:02 +0200

    add minutes and seconds countdown)

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,27 +2,29 @@ import { useState } from "react"
 
 const ButtonsContainer = ({timePomodoro, timeShortBreak, timeLongBreak}) => {
 
-    const [time, setTime] = useState(timePomodoro)
+    let [minutes, setMinutes] = useState(timePomodoro)
+    let [seconds, setSeconds] = useState(60)
     const [buttonStatePomodoro, setButtonStatePomodoro] = useState(true)
     const [buttonStateShortBreak, setButtonStateShortBreak] = useState(false)
     const [buttonStateLongBreak, setButtonStateLongBreak] = useState(false)
-
+    let interval;
+    
     const handleClickPomodoro = () => {
-        setTime(timePomodoro)
+        setMinutes(timePomodoro)
         setButtonStatePomodoro(true)
         setButtonStateShortBreak(false)
         setButtonStateLongBreak(false)
     }
 
     const handleClickShortBreak = () => {
-        setTime(timeShortBreak)
+        setMinutes(timeShortBreak)
         setButtonStateShortBreak(true)
         setButtonStatePomodoro(false)
         setButtonStateLongBreak(false)
     }
 
     const handleClickLongBreak = () => {
-        setTime(timeLongBreak)
+        setMinutes(timeLongBreak)
         setButtonStateLongBreak(true)
         setButtonStatePomodoro(false)
         setButtonStateShortBreak(false)
@@ -31,6 +33,32 @@ const ButtonsContainer = ({timePomodoro, timeShortBreak, timeLongBreak}) => {
     let toggleClassCheck = buttonStatePomodoro ? 'active-button' : 'inactive-button'
     let toggleClassCheckShortBreak = buttonStateShortBreak ? 'active-button' : 'inactive-button'
     let toggleClassCheckLongBreak = buttonStateLongBreak ? 'active-button' : 'inactive-button'
+
+    
+
+    const handleStartClick = () => {
+        interval = setInterval(countDown, 1000)
+        countDown()
+    }
+
+    function countDown() {
+        setSeconds(seconds--)
+        
+        if (seconds === '00') {
+            setSeconds(60)
+        }
+
+        if (seconds === 0) {
+            setMinutes(minutes--)
+            setSeconds('60')
+        }
+
+        if (minutes === 0) {
+            clearInterval(interval)
+        }
+
+        console.log(minutes, seconds)
+    }
 
     return (
         <>
@@ -56,10 +84,12 @@ const ButtonsContainer = ({timePomodoro, timeShortBreak, timeLongBreak}) => {
             </div>
             <div className='circle'>
                 <div className='circle-line-time'>
-                <p className='circle-line-time-pomodoro'>
-                    {time}
-                </p>
-                <button className='circle-line-time-button-start'>start</button>
+                <div className='circle-line-time-pomodoro'>
+                    {minutes}:{seconds}
+                </div>
+                <button
+                    onClick={handleStartClick}
+                    className='circle-line-time-button-start'>start</button>
                 </div>
             </div>
         </>
