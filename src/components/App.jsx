5aca6541ce568ca6b3e83573a4e5e0ac3f3fe8c4
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-05-05 12:54:13 +0200

    add progress bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,43 +2,34 @@ import '../styles/App.scss'
 import Homepage from './Homepage'
 import Header from './Header'
 import Modal from './Modal'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 
 function App() {
-    // from here Time section
+    // from here **************************** ----> TIME SECTION <---- **************************** //
+
+    /* variables from the form/modal */
     const [timePomodoro, setTimePomodoro] = useState(25)
     const [timeShortBreak, setTimeShortBreak] = useState(5)
     const [timeLongBreak, setTimeLongBreak] = useState(15)
-    // end here Time section
 
-    /* ************************************************************* */
+    /* minutes and seconds in the circle */
+    const [minutes, setMinutes] = useState(timePomodoro)
+    const [seconds, setSeconds] = useState(0)
 
-    // from here Font section
-    const [firstFont, setFirstFont] = useState(true)
-    const [secondFont, setSecondFont] = useState(false)
-    const [thirdFont, setThirdFont] = useState(false)
-    
-    const [classFirstFont, setClassFirstFont] = useState('kumbh-sans-font')
-    const [classSecondFont, setClassSecondFont] = useState('')
-    const [classThirdFont, setClassThirdFont] = useState('')
-    // end here Font section
+    /* buttonState to mark the button when clicked */
+    const [buttonStatePomodoro, setButtonStatePomodoro] = useState(true)
+    const [buttonStateShortBreak, setButtonStateShortBreak] = useState(false)
+    const [buttonStateLongBreak, setButtonStateLongBreak] = useState(false)
 
-    /* ************************************************************* */
+    /* interval and textButton for the countDown */
+    const interval = useRef(0)
+    const [textButton, setTextButton] = useState('start')
 
-    // from here Color section
-    const [firstColor, setFirstColor] = useState(true)
-    const [secondColor, setSecondColor] = useState(false)
-    const [thirdColor, setThirdColor] = useState(false)
-
-    const [classFirstColor, setClassFirstColor] = useState('first-color-background')
-    const [classSecondColor, setClassSecondColor] = useState('')
-    const [classThirdColor, setClassThirdColor] = useState('')
-    // end here Color Section
-
-    /* ************************************************************* */
 
     // from here Time functions
+
+    /* handleTime functions to change time based on the form/modal */
     const handleTimePomodoro = (value) => {
         setTimePomodoro(value)
     }
@@ -50,9 +41,135 @@ function App() {
     const handleTimeLongBreak = (value) => {
         setTimeLongBreak(value)
     }
+
+    // -------------------- //
+    
+    /* handleClick to change color buttons when clicked and to manage reset button */
+    const handleClickPomodoro = () => {
+        resetCountDown(timePomodoro, 0)
+        // setMinutes(timePomodoro) // write this when click on button apply
+        setButtonStatePomodoro(true)
+        setButtonStateShortBreak(false)
+        setButtonStateLongBreak(false)
+        setTextButton('start')
+    }
+
+    const handleClickShortBreak = () => {
+        resetCountDown(timeShortBreak, 0)
+        // setMinutes(timeShortBreak) // write this when click on button apply
+        // setSeconds(0)
+        setButtonStateShortBreak(true)
+        setButtonStatePomodoro(false)
+        setButtonStateLongBreak(false)
+        setTextButton('start')
+    }
+
+    const handleClickLongBreak = () => {
+        resetCountDown(timeLongBreak, 0)
+        // setMinutes(timeLongBreak) // write this when click on button apply
+        // setSeconds(0)
+        setButtonStateLongBreak(true)
+        setButtonStatePomodoro(false)
+        setButtonStateShortBreak(false)
+        setTextButton('start')
+    }
+
+    // -------------------- //
+
+    /* variables to change css when a button is active or inactive */
+    let toggleClassCheck = buttonStatePomodoro ? 'active-button' : 'inactive-button'
+    let toggleClassCheckShortBreak = buttonStateShortBreak ? 'active-button' : 'inactive-button'
+    let toggleClassCheckLongBreak = buttonStateLongBreak ? 'active-button' : 'inactive-button'
+
+    // -------------------- //
+
+    if (seconds < 0) {
+        if (minutes !== 0) {
+            setMinutes(minutes => minutes -1)
+            setSeconds(59)
+        }
+    }
+
+    let startingPoint = 100
+    let totalSeconds = minutes * 60
+    console.log(minutes)
+
+    // coefficente che sottraggo al 100% per sincronizzare il tempo con la progress bar
+    let nameToChange = 100 / totalSeconds
+
+    const orangeColor = '#F87070'
+    const blueColor = '#1E213F'
+    const greyColor = '#D7E0FF'
+
+    const changePercentBackground = () => {
+        document.getElementById('enrica').style.background = `radial-gradient(closest-side, ${blueColor} 95%, transparent 80% 100%), conic-gradient(${orangeColor} ${startingPoint -= nameToChange}%, ${greyColor} 0)`
+    }
+    
+    const countDown = () => {
+        interval.current = setInterval(() => {
+            setSeconds(seconds => seconds -1)
+            changePercentBackground()
+        }, 1000)
+    }
+
+    /* 
+    function resetCountDown
+    @param (minutes) reset the countDown minutes
+    @param (seconds) reset the countDown seconds
+    */
+    const resetCountDown = (minutes, seconds) => {
+        clearInterval(interval.current)
+        setMinutes(minutes)
+        setSeconds(seconds)
+    }
+    
+
+    const handleStartRestartClick = () => {
+        if (textButton === 'start') {
+            countDown()
+            setTextButton('restart')
+        } 
+        if (textButton === 'restart') {
+
+            if (buttonStatePomodoro) {
+                resetCountDown(timePomodoro, 0)
+            }
+
+            if (buttonStateShortBreak) {
+                resetCountDown(timeShortBreak, 0)
+            }
+
+            if (buttonStateLongBreak) {
+                resetCountDown(timeLongBreak, 0)
+            }
+            
+            setTextButton('start')
+        }
+    }
+
     // end here Time functions
 
     /* ************************************************************* */
+
+    // from here **************************** ----> FONT SECTION <---- **************************** //
+    const [firstFont, setFirstFont] = useState(true)
+    const [secondFont, setSecondFont] = useState(false)
+    const [thirdFont, setThirdFont] = useState(false)
+    
+    const [classFirstFont, setClassFirstFont] = useState('kumbh-sans-font')
+    const [classSecondFont, setClassSecondFont] = useState('')
+    const [classThirdFont, setClassThirdFont] = useState('')
+
+    // from here Color section
+    const [firstColor, setFirstColor] = useState(true)
+    const [secondColor, setSecondColor] = useState(false)
+    const [thirdColor, setThirdColor] = useState(false)
+
+    const [classFirstColor, setClassFirstColor] = useState('first-color-background')
+    const [classSecondColor, setClassSecondColor] = useState('')
+    const [classThirdColor, setClassThirdColor] = useState('')
+
+    // -------------------- //
 
     // from here Font functions
     const handleClickFirstFont = (ev) => {
@@ -92,7 +209,7 @@ function App() {
 
     /* ************************************************************* */
 
-    // from here Color functions
+    // from here **************************** ----> COLOR SECTION <---- **************************** //
     const handleClickFirstColor = () => {
         setFirstColor(true)
         setSecondColor(false)
@@ -120,6 +237,8 @@ function App() {
         setClassSecondColor('')
     }
 
+    // -------------------- //
+
     const toggleFirstColor = firstColor ? 'active-color' : 'inactive-color'
     const toggleSecondColor = secondColor ? 'active-color' : 'inactive-color'
     const toggleThirdColor = thirdColor ? 'active-color' : 'inactive-color'
@@ -128,7 +247,8 @@ function App() {
     const updateSecondColor = classSecondColor ? 'second-color-background' : ''
     const updateThirdColor = classThirdColor ? 'third-color-background' : ''
     // end here Color functions
-    
+
+    /* ************************************************************* */
     
   return (
     <div className='main'>
@@ -137,6 +257,19 @@ function App() {
         timePomodoro={timePomodoro}
         timeShortBreak={timeShortBreak}
         timeLongBreak={timeLongBreak}
+
+        minutes={minutes}
+        seconds={seconds}
+
+        toggleClassCheck={toggleClassCheck}
+        toggleClassCheckShortBreak={toggleClassCheckShortBreak}
+        toggleClassCheckLongBreak={toggleClassCheckLongBreak}
+
+        handleClickPomodoro={handleClickPomodoro}
+        handleClickShortBreak={handleClickShortBreak}
+        handleClickLongBreak={handleClickLongBreak}
+        handleStartRestartClick={handleStartRestartClick}
+        textButton={textButton}
 
         updateFirstFont={updateFirstFont}
         updateSecondFont={updateSecondFont}
@@ -151,6 +284,7 @@ function App() {
         timePomodoro={timePomodoro}
         timeShortBreak={timeShortBreak}
         timeLongBreak={timeLongBreak}
+        
         handleTimePomodoro={handleTimePomodoro}
         handleTimeShortBreak={handleTimeShortBreak}
         handleTimeLongBreak={handleTimeLongBreak}
@@ -182,7 +316,6 @@ function App() {
         updateFirstColor={updateFirstColor}
         updateSecondColor={updateSecondColor}
         updateThirdColor={updateThirdColor}
-
       >
       </Modal>
     </div>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,92 +1,4 @@
-import { useRef, useState } from 'react'
-
-const Homepage = ({timePomodoro, timeShortBreak, timeLongBreak, updateFirstFont, updateSecondFont, updateThirdFont, updateFirstColor, updateSecondColor, updateThirdColor}) => {
-
-    const [minutes, setMinutes] = useState(timePomodoro)
-    const [seconds, setSeconds] = useState(0)
-    const [buttonStatePomodoro, setButtonStatePomodoro] = useState(true)
-    const [buttonStateShortBreak, setButtonStateShortBreak] = useState(false)
-    const [buttonStateLongBreak, setButtonStateLongBreak] = useState(false)
-    const interval = useRef(0)
-    const [textButton, setTextButton] = useState('start')
-    
-    const handleClickPomodoro = () => {
-        resetCountDown(timePomodoro, 0)
-        // setMinutes(timePomodoro) // write this when click on button apply
-        setButtonStatePomodoro(true)
-        setButtonStateShortBreak(false)
-        setButtonStateLongBreak(false)
-        setTextButton('start')
-    }
-
-    const handleClickShortBreak = () => {
-        resetCountDown(timeShortBreak, 0)
-        // setMinutes(timeShortBreak) // write this when click on button apply
-        // setSeconds(0)
-        setButtonStateShortBreak(true)
-        setButtonStatePomodoro(false)
-        setButtonStateLongBreak(false)
-        setTextButton('start')
-    }
-
-    const handleClickLongBreak = () => {
-        resetCountDown(timeLongBreak, 0)
-        // setMinutes(timeLongBreak) // write this when click on button apply
-        // setSeconds(0)
-        setButtonStateLongBreak(true)
-        setButtonStatePomodoro(false)
-        setButtonStateShortBreak(false)
-        setTextButton('start')
-    }
-
-    let toggleClassCheck = buttonStatePomodoro ? 'active-button' : 'inactive-button'
-    let toggleClassCheckShortBreak = buttonStateShortBreak ? 'active-button' : 'inactive-button'
-    let toggleClassCheckLongBreak = buttonStateLongBreak ? 'active-button' : 'inactive-button'
-
-    if (seconds < 0) {
-
-        if (minutes !== 0) {
-            setMinutes(minutes => minutes -1)
-            setSeconds(59)
-        }
-
-    }
-
-    const countDown = () => {
-        interval.current = setInterval(() => {
-            setSeconds(seconds => seconds -1)
-            }, 1000)
-    }
-
-    const resetCountDown = (minutes, seconds) => {
-        clearInterval(interval.current)
-        setMinutes(minutes)
-        setSeconds(seconds)
-    }
-    
-
-    const handleStartRestartClick = () => {
-        if (textButton === 'start') {
-            countDown()
-            setTextButton('restart')
-        } 
-        if (textButton === 'restart') {
-
-            if (buttonStatePomodoro) {
-                resetCountDown(timePomodoro, 0)
-            }
-
-            if (buttonStateShortBreak) {
-                resetCountDown(timeShortBreak, 0)
-            }
-
-            if (buttonStateLongBreak) {
-                resetCountDown(timeLongBreak, 0)
-            }
-            
-            setTextButton('start')
-        }
-    }
+const Homepage = ({updateFirstFont, updateSecondFont, updateThirdFont, toggleClassCheck, toggleClassCheckShortBreak, toggleClassCheckLongBreak, handleClickPomodoro, handleClickShortBreak, handleClickLongBreak, minutes, seconds, handleStartRestartClick, textButton}) => {
 
     return (
         <>
@@ -111,14 +23,14 @@ const Homepage = ({timePomodoro, timeShortBreak, timeLongBreak, updateFirstFont,
                 </button>
             </div>
             <div className='circle'>
-                <div className='circle-line-time'>
-                <div className={`circle-line-time-pomodoro ${updateFirstFont} ${updateSecondFont} ${updateThirdFont}`}>
-                    {minutes < 10 ? '0'+minutes : minutes}:{seconds < 10 ? '0'+seconds : seconds}
-                </div>
-                <button
-                    onClick={handleStartRestartClick}
-                    className={`circle-line-time-button-start ${updateFirstFont} ${updateSecondFont} ${updateThirdFont}`}>{textButton}
-                </button>
+                <div className='circle-line-time' id='enrica'>
+                    <div className={`circle-line-time-pomodoro ${updateFirstFont} ${updateSecondFont} ${updateThirdFont}`}>
+                        {minutes < 10 ? '0'+minutes : minutes}:{seconds < 10 ? '0'+seconds : seconds}
+                    </div>
+                    <button
+                        onClick={handleStartRestartClick}
+                        className={`circle-line-time-button-start ${updateFirstFont} ${updateSecondFont} ${updateThirdFont}`}>{textButton}
+                    </button>
                 </div>
             </div>
         </>
